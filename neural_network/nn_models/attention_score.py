adc0d1256c7824c8ffb83e51dc94d9202af7ee8d
Isaac Sim <gilgarad@igsinc.co.kr>
2019-03-14 17:48:34 +0900

    generate models with single loss and multi loss supported

--- neural_network/nn_models/attention_score.py
+++ neural_network/nn_models/attention_score.py
@@ -1,5 +1,5 @@
 from keras.layers import Multiply, Dense, Permute, Lambda, Input, Dropout, Reshape, Flatten, Conv1D, \
-    BatchNormalization, Activation
+    BatchNormalization, Activation, Average
 
 # from keras.layers.core import *
 from keras.models import Model
@@ -11,7 +11,7 @@ from neural_network.utils.custom_function import ScoreActivationFromSigmoid, Get
 
 
 # Best Working for Fourier Transform version
-class AttentionScoreFourierTransform:
+class AttentionScoreFourierTransform: # Single loss
     def __init__(self, initial_params, gpu=0):
         input_shape, output_labels = initial_params
         with K.tf.device('/gpu:' + str(gpu)):
@@ -46,11 +46,12 @@ class AttentionScoreFourierTransform:
 
             model = Model(inputs=inputs, outputs=all_final_scores)
 
-        losses = {'amusement': 'mean_squared_error',
-                  'immersion': 'mean_squared_error',
-                  'difficulty': 'mean_squared_error',
-                  'emotion': 'mean_squared_error'}
-        loss_weights = {'amusement': 1.0, 'immersion': 1.0, 'difficulty': 1.0, 'emotion': 1.0}
+        losses = dict()
+        loss_weights = dict()
+        for label_score in output_labels:
+            losses[label_score] = 'mean_squared_error'
+            loss_weights[label_score] = 1.0
+
         adam = optimizers.Adam(lr=0.001)
         model.compile(loss=losses, loss_weights=loss_weights, optimizer=adam, metrics=['accuracy'])
 
@@ -69,9 +70,75 @@ class AttentionScoreFourierTransform:
 
     @staticmethod
     def get_initial_params(x_train, y_train):
-        label_names = ['amusement', 'immersion', 'difficulty', 'emotion']
+        #         label_names = ['amusement', 'immersion', 'difficulty', 'emotion']
+        label_names = ['amusement']
         input_shape = (x_train.shape[1], x_train.shape[2])
-        return [input_shape]
+        return [input_shape, label_names]
+
+
+class AttentionScoreFourierTransformMultiLoss:
+    def __init__(self, initial_params, gpu=0):
+        input_shape, output_labels = initial_params
+        with K.tf.device('/gpu:' + str(gpu)):
+            inputs = Input(shape=input_shape)
+
+            num_attentions = 64
+            num_neurons = 128
+
+            m = inputs
+            mask = Lambda(lambda x: GetPadMask(x))(m)
+            nonzero_mask_count = Lambda(lambda x: GetCountNonZero(x))(mask)
+
+            m = Dense(num_neurons, activation='relu')(m)
+            m = Dropout(0.7)(m)
+
+            all_final_scores = list()
+            for label_score in output_labels:
+                _m = Dense(num_attentions, activation=ScoreActivationFromSigmoid)(m)  # n attentions
+                _m = Multiply()([mask, _m])
+                _m = Multiply()([_m, nonzero_mask_count])
+
+                _m = Permute((2, 1))(_m)
+
+                _m = Lambda(lambda x: K.sum(x, axis=2))(_m)  # each attention's average
+
+                _m = Reshape((num_attentions, 1))(_m)
+                final_score = Lambda(lambda x: K.mean(x, axis=1, keepdims=True))(
+                    _m)  # average of all attention's averages
+                #                 final_score = Multiply(name=label_score)([final_score, Lambda(lambda x: K.ones_like(x))(final_score)])
+                final_score = Flatten(name=label_score)(final_score)
+                all_final_scores.append(final_score)
+
+            model = Model(inputs=inputs, outputs=all_final_scores)
+
+        losses = dict()
+        loss_weights = dict()
+        for label_score in output_labels:
+            losses[label_score] = 'mean_squared_error'
+            loss_weights[label_score] = 1.0
+
+        adam = optimizers.Adam(lr=0.001)
+        model.compile(loss=losses, loss_weights=loss_weights, optimizer=adam, metrics=['accuracy'])
+
+        self.model = model
+        print(model.summary())
+
+    def train(self, x_train, y_train, x_test, y_test, epochs=100, batch_size=128, verbose=1):
+        print('##########')
+        print('MLP Attention Test')
+        print('##########')
+        #         early_stopping = EarlyStopping(patience = 20)
+        early_stopping = LrReducer(patience=5, reduce_rate=0.5, reduce_nb=3, verbose=1)
+        self.model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=batch_size,
+                       verbose=verbose,
+                       callbacks=[early_stopping])
+
+    @staticmethod
+    def get_initial_params(x_train, y_train):
+        #         label_names = ['amusement', 'immersion', 'difficulty', 'emotion']
+        label_names = ['amusement', 'immersion', 'difficulty']
+        input_shape = (x_train.shape[1], x_train.shape[2])
+        return [input_shape, label_names]
 
 
 # For rawdata
@@ -136,3 +203,73 @@ class AttentionScore:
     def get_initial_params(x_train, y_train):
         input_shape = (x_train.shape[1], x_train.shape[2])
         return [input_shape]
+
+
+class AttentionScoreMultiloss:
+    def __init__(self, initial_params, gpu=0):
+        input_shape, output_labels = initial_params
+        with K.tf.device('/gpu:' + str(gpu)):
+            inputs = Input(shape=input_shape)
+
+            num_attentions = 128
+            num_filters = num_attentions  # used in conv1d
+            kernel_size = 128 * 1
+            strides = 128 * 1
+
+            m = inputs
+
+            m = Conv1D(num_filters, kernel_size=kernel_size, strides=strides, padding='valid', use_bias=False)(m)
+            mask_conv = Lambda(lambda x: GetPadMask(x))(m)
+            nonzero_mask_count = Lambda(lambda x: GetCountNonZero(x))(mask_conv)
+            m = BatchNormalization()(m)
+
+            all_final_scores = list()
+            for label_score in output_labels:
+                _m = Dense(num_filters)(m)
+                _m = Activation(ScoreActivationFromSigmoid)(_m)
+                _m = Multiply()([mask_conv, _m])
+                m2 = Multiply()([nonzero_mask_count, _m])
+
+                _m = Permute((2, 1))(_m)
+                m2 = Permute((2, 1))(m2)
+
+                final_score1 = Lambda(lambda x: K.sum(x, axis=2))(m2)  # each attention's average
+                final_score1 = Reshape((num_attentions, 1))(final_score1)
+
+                final_score2 = Dense(1)(_m)
+                final_score = Average()([final_score1, final_score2])
+
+                final_score = Lambda(lambda x: K.mean(x, axis=1, keepdims=True))(
+                    final_score)  # average of all attention's averages
+                final_score = Flatten(name=label_score)(final_score)
+                all_final_scores.append(final_score)
+
+            model = Model(inputs=inputs, outputs=all_final_scores)
+
+        losses = dict()
+        loss_weights = dict()
+        for label_score in output_labels:
+            losses[label_score] = 'mean_squared_error'
+            loss_weights[label_score] = 1.0
+
+        adam = optimizers.Adam(lr=0.001)
+        model.compile(loss=losses, loss_weights=loss_weights, optimizer=adam, metrics=['accuracy'])
+
+        self.model = model
+        print(model.summary())
+
+    def train(self, x_train, y_train, x_test, y_test, epochs=100, batch_size=128, verbose=1):
+        print('##########')
+        print('MLP Attention Test')
+        print('##########')
+        #         early_stopping = EarlyStopping(patience = 20)
+        early_stopping = LrReducer(patience=5, reduce_rate=0.5, reduce_nb=3, verbose=1)
+        self.model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=batch_size,
+                       verbose=verbose,
+                       callbacks=[early_stopping])
+
+    @staticmethod
+    def get_initial_params(x_train, y_train):
+        label_names = ['amusement', 'immersion', 'difficulty', 'emotion']
+        input_shape = (x_train.shape[1], x_train.shape[2])
+        return [input_shape, label_names]
